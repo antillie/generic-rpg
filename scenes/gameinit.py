676568105f51e448bfa89fedc8358ed3af4b3231
antillie <antillie@gmail.com>
2017-02-10 01:34:15 -0600

    scrolling map support added

--- scenes/gameinit.py
+++ scenes/gameinit.py
@@ -5,6 +5,32 @@ import pygame
 import virtualscreen
 import base
 import colors
+import pyscroll
+import pytmx.util_pygame
+import os
+import utils
+
+class Car(pygame.sprite.Sprite):
+    #This class represents a car. It derives from the "Sprite" class in Pygame.
+    
+    def __init__(self, color=colors.brown, width=32, height=48):
+        # Call the parent class (Sprite) constructor
+        super(Car, self).__init__()
+        
+        # Pass in the color of the car, and its x and y position, width and height.
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width, height])
+        self.image.fill(colors.brown)
+        self.image.set_colorkey(colors.brown)
+ 
+        # Draw the car (a rectangle!)
+        pygame.draw.rect(self.image, colors.brown, [0, 0, width, height])
+        
+        # Instead we could load a proper pciture of a car...
+        # self.image = pygame.image.load("car.png").convert_alpha()
+ 
+        # Fetch the rectangle object that has the dimensions of the image.
+        self.rect = self.image.get_rect()
 
 # The actual game. Different versions of this class will need to load maps, characters, dialog, and detect interactions between objects on the screen. Each area will be its own class.
 class GameScene(base.SceneBase):
@@ -18,6 +44,22 @@ class GameScene(base.SceneBase):
         self.rect_x = 512
         self.rect_y = 288
         
+        path = os.path.dirname(os.path.realpath(__file__)) + "/maps/initial.tmx"
+        path = path.replace('/', os.sep).replace("\\", os.sep)
+        self.tmx_data = pytmx.util_pygame.load_pygame(path)
+        self.map_data = pyscroll.TiledMapData(self.tmx_data)
+        
+        screen_size = (1024, 576)
+        map_layer = pyscroll.BufferedRenderer(self.map_data, screen_size)
+        
+        self.group = pyscroll.PyscrollGroup(map_layer=map_layer)
+        
+        self.player = Car()
+        
+        self.all_sprites_list = pygame.sprite.Group()
+        
+        self.all_sprites_list.add(self.player)
+        
     # Handles user input passed from the main engine.
     def ProcessInput(self, events, pressed_keys):
         # Play the town theme for now.
@@ -30,13 +72,13 @@ class GameScene(base.SceneBase):
                     self.SwitchToScene("PartyScreen")
                 
         if pressed_keys[pygame.K_UP]:
-            self.rect_y = self.rect_y - 5
+            self.rect_y = self.rect_y - 3
         if pressed_keys[pygame.K_DOWN]:
-            self.rect_y = self.rect_y + 5
+            self.rect_y = self.rect_y + 3
         if pressed_keys[pygame.K_LEFT]:
-            self.rect_x = self.rect_x - 5
+            self.rect_x = self.rect_x - 3
         if pressed_keys[pygame.K_RIGHT]:
-            self.rect_x = self.rect_x + 5
+            self.rect_x = self.rect_x + 3
                 
         if self.rect_x < 0:
             self.rect_x = 0
@@ -50,7 +92,11 @@ class GameScene(base.SceneBase):
         
     # Internal game logic.
     def Update(self):
-        pass
+        
+        self.player.rect.top = self.rect_y
+        self.player.rect.left = self.rect_x
+        
+        self.all_sprites_list.update()
     
     # Draws things.
     def Render(self, screen, real_w, real_h):
@@ -58,15 +104,19 @@ class GameScene(base.SceneBase):
         # Create our staticly sized virtual screen so we can draw stuff on it.
         canvas = virtualscreen.VirtualScreen(real_w, real_h)
         
-        # Filling a 1024x576 screen requires 16x9 64x64 pixel tiles.
-        x_count = 16
-        y_count = 9
+        scaled_w = self.player.rect.center[0] * 1.6
+        scaled_h = self.player.rect.center[1] * 1.6
+        new_center = (scaled_w, scaled_h)
         
-        for y in range(y_count):
-            for x in range(x_count):
-                canvas.canvas.blit(self.cache.get_image("landscaping/mountain_landscape.png"), (x * 64, y * 64), (448, 128, 64, 64))
+        self.group.center(new_center)
         
-        pygame.draw.rect(canvas.canvas, colors.brown, pygame.Rect(self.rect_x, self.rect_y, 32, 48))
+        self.group.add(self.player)
+        
+        self.group.draw(canvas.canvas)
+        
+        self.all_sprites_list.draw(canvas.canvas)
+        
+        pygame.draw.rect(canvas.canvas, colors.brown, self.player.rect)
         
         # Draw the upscaled virtual screen to actual screen.
         screen.blit(canvas.render(), (0, 0))
